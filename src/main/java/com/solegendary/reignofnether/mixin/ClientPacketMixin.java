89ed11d6ea0a501f5c064a725faacdc499e72695
2b2m-org <283744213+2b2m-org@users.noreply.github.com>
2026-07-13 04:54:42 -0400

    fix: access client state after packet listener split

--- src/main/java/com/solegendary/reignofnether/mixin/ClientPacketMixin.java
+++ src/main/java/com/solegendary/reignofnether/mixin/ClientPacketMixin.java
@@ -10,7 +10,6 @@ import net.minecraft.client.multiplayer.ClientPacketListener;
 import net.minecraft.network.protocol.game.ClientboundSetTimePacket;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -20,20 +19,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPacketListener.class)
 public class ClientPacketMixin {
-
-    @Shadow Minecraft minecraft;
-
     @Inject(
             method = "handleSetTime",
             at = @At("HEAD"),
             cancellable = true
     )
     private void handleSetTime(ClientboundSetTimePacket pPacket, CallbackInfo ci) {
+        Minecraft minecraft = Minecraft.getInstance();
         Vec3 pos;
         if (OrthoviewClientEvents.isEnabled())
-            pos = MiscUtil.getOrthoviewCentreWorldPos(this.minecraft);
-        else if (this.minecraft.player != null && this.minecraft.level != null)
-            pos = this.minecraft.player.position();
+            pos = MiscUtil.getOrthoviewCentreWorldPos(minecraft);
+        else if (minecraft.player != null && minecraft.level != null)
+            pos = minecraft.player.position();
         else
             return;
 
